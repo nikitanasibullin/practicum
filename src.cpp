17840f22932bbe64208cc32728873b53e9ff57d6
nikitanasibullin <73995578+nikitanasibullin@users.noreply.github.com>
2023-10-10 14:42:32 +0300

    Update src.cpp

--- src.cpp
+++ src.cpp
@@ -11,13 +11,13 @@ int main()
 	std::cin >> a >> b >> c;
 
 	double D = std::sqrt(b*b - 4 * a*c);
-	if (a == 0) { std::cout << "x=" << -c / b << std::endl; }
+	if (a == 0) { std::cout << "x = " << -c / b << std::endl; }
 	else {
 		if (D>0) {
-			std::cout << "x1=" << (-b + D) / (2 * a) << "       x2=" << (-b - D) / (2 * a) << std::endl;
+			std::cout << "x1 = " << (-b + D) / (2 * a) << "       x2 = " << (-b - D) / (2 * a) << std::endl;
 		}
 		else {
-			if (D == 0) { std::cout << "x=" << (-b + D) / (2 * a) << std::endl; }
+			if (D == 0) { std::cout << "x = " << (-b + D) / (2 * a) << std::endl; }
 			else { std::cout << "none" << std::endl; }
 		}
 	}
